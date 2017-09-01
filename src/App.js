95ecfa9a3ae797b03e2127ad0b93d9398839357a
tcarlton2000 <tcarlton2000@gmail.com>
2017-09-01 04:06:03 -0500

    Revert "Remove Recipes"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Recipes from "./Recipes.js"
 import Ingredients from "./Ingredients.js"
 import { Navbar, Nav, NavItem } from "react-bootstrap";
 
@@ -20,6 +21,14 @@ class App extends Component {
             </Navbar.Brand>
           </Navbar.Header>
           <Nav>
+            <NavItem
+              id="recipeNavButton"
+              eventKey={3}
+              href="#"
+              onClick={() => this.setState({ view: <Recipes /> })}
+            >
+              Recipes
+            </NavItem>
             <NavItem
               id="ingredientsNavButton"
               eventKey={4}
